45c95db1bf516f660b4ed3572879c326369f74ff
Denchic205 <denchiczerofive20@gmail.com>
2021-01-19 16:27:57 +0300

    New victory sound! Sounds good!

--- 1.py
+++ 1.py
@@ -49,10 +49,6 @@ class SpriteGroup(pygame.sprite.Group):
     def __init__(self):
         super().__init__()
 
-    def get_event(self, event):
-        for sprite in self:
-            sprite.get_event(event)
-
 
 class Particle(pygame.sprite.Sprite):
     def __init__(self, pos, dx, dy):
